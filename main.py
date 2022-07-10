33801b8d41a603f4dca6fa2fa4ee6442e6dee1ed
Zack Ni <yn117@scarletmail.rutgers.edu>
2022-07-10 11:58:51 -0400

    finish up the feature

--- main.py
+++ main.py
@@ -1,49 +1,30 @@
-# please input the python path you want to use
-from glob import glob
-
-
 python_path = "python"
 
-# detect if a package is installed
-def is_package_installed(package):
-    global python_path
+def cp_file_recursively(src, dst):
     import subprocess
     try:
-        result = subprocess.check_output([python_path, "-m", "pip", "list"])
-        if package + " " in str(result):
-            return True
-        else:
-            return False
+        result = subprocess.check_output([python_path, "-m", "awscli", "s3", "cp", src, dst, "--recursive"])
     except FileNotFoundError:
         return False
+    return True
 
-# install a package
-def install_package(package):
-    global python_path
+def rm_file_recursively(src):
     import subprocess
-    subprocess.call([python_path, "-m", "pip", "install", package])
+    try:
+        result = subprocess.check_output([python_path, "-m", "awscli", "s3", "rm", src, "--recursive"])
+    except FileNotFoundError:
+        return False
+    return True
 
-# is awscli installed
-if is_package_installed("awscli"):
-    print("awscli is installed")
-else:
-    print("awscli is not installed")
-    print("installing awscli")
-    install_package("awscli")
+def generate_s3_bucket_url(bucket_name):
+    return "s3://" + bucket_name
 
-import awscli
-from awscli.customizations.s3.s3 import S3
-from awscli.customizations.s3.subcommands import ListCommand, WebsiteCommand
-from awscli.customizations.s3.subcommands import CpCommand, MvCommand, RmCommand
-from botocore import session
-# from awscli.clidriver import main
-from awscli.customizations.configure.configure import ConfigureCommand
+# read from crednetial.json
+import json
+with open("credential.json") as f:
+    data = json.load(f)
 
-session = session.get_session()
-
-configure = ConfigureCommand(session)
-
-configure([], None)
-
-# main()
-# session = session.get_session()
+bucket_name = data["bucket_name"]
+local_path = data["local_path"]
+rm_file_recursively(generate_s3_bucket_url(bucket_name) )
+cp_file_recursively(local_path, generate_s3_bucket_url(bucket_name))
